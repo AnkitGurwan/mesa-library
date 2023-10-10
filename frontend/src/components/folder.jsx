2463abd3338332da6c7c124db9f2ac3c99c1dfe2
DeveshKamble <d.kamble@iitg.ac.in>
2023-10-10 13:03:59 +0530

    UI changes

--- frontend/src/components/folder.jsx
+++ frontend/src/components/folder.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { setPath } from '../redux/storage/storageSlice';
+import folder from './images/folder.png';
 
 const Folder = (props) => {
   const dispatch = useDispatch();
@@ -10,9 +11,11 @@ const Folder = (props) => {
     dispatch(setPath(props.name));
   }
   return (
-    <Link to={`${props.name}`} onClick={clickHandler} className='py-6 mx-4 border overflow-y-hidden w-24 h-24 flex justify-between items-center cursor-pointer hover:bg-gray-100 flex-col'>
-        <i class="fa-regular fa-folder px-2 text-2xl font-light"></i>
-        <div className='text-xs px-1 pt-2 capitalize'>{props.name}</div>
+    
+    <Link to={`${props.name}`} onClick={clickHandler} className='py-3 mx-2 rounded-xl overflow-y-hidden w-24 h-24 flex justify-between items-center cursor-pointer hover:bg-gray-100 flex-col'>
+        <img src={folder} alt="folder" className='' />
+        {/* <i class="fa-regular fa-folder px-2 text-2xl font-light"></i> */}
+        <div className='text-xs px-1 pt- capitalize'>{props.name}</div>
     </Link>
   )
 }
