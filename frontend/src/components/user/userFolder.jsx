0c4cc8d672892a99e749bde1abc6103dda99dd31
DeveshKamble <d.kamble@iitg.ac.in>
2023-10-15 15:55:57 +0530

    minor changes

--- frontend/src/components/user/userFolder.jsx
+++ frontend/src/components/user/userFolder.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { setUserPath } from '../../redux/storage/storageSlice';
+import folder from '../images/folder.png'
 import "react-toastify/dist/ReactToastify.css";
 
 const Folder = (props) => {
@@ -12,7 +13,7 @@ const Folder = (props) => {
   }
 
   return (
-    <Link to={`${props.name}`} onClick={clickHandler} className='folder py-3 mx-2 rounded-xl  w-24 h-24 flex justify-between items-center cursor-pointer hover:bg-transparent hover:border-2 hover:border-transparent flex-col'>
+    <Link to={`${props.name}`} onClick={clickHandler} className='folder py-3 mx-2 w-24 h-24 flex justify-between items-center cursor-pointer flex-col transition ease-in-out delay-150 hover:-translate-y-1 hover:-translate-x-1'>
         <img src={folder} alt="folder" className='w-[64px]' />
         {/* <i class="fa-regular fa-folder px-2 text-2xl font-light"></i> */}
         <div className='text-[17px] font-normal text-center px-1 pt-1 capitalize'>{props.name}</div>
